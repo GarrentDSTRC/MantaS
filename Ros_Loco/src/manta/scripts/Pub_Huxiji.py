e6287ff1343f1136cae0c919644964d33de1cd1b
Handsome_Icesticke <94521069+GarrentDSTRC@users.noreply.github.com>
2024-10-21 15:45:30 +0800

    1

--- Ros_Loco/src/manta/scripts/Pub_Huxiji.py
+++ Ros_Loco/src/manta/scripts/Pub_Huxiji.py
@@ -5,46 +5,33 @@ from std_msgs.msg import String, Float32
 
 def publish_steps():
     rospy.init_node('step_publisher', anonymous=True)
-    position_pub = rospy.Publisher('/absolute_position', Float32, queue_size=10)
-    description_pub = rospy.Publisher('/step_description', String, queue_size=10)
+    position_pub = rospy.Publisher('/relative_position', Float32, queue_size=10)
+    description_pub = rospy.Publisher('/absolute_description', Float32, queue_size=10)
     
     steps = [
-        (0, "开状态"),
-        (-90, "一轮测量（关状态）"),
-        (0, "换水"),
-        (90, "二轮测量"),
-        (180, "换水"),
-        (90, "三轮测量"),
-        (0, "换水"),
-        (90, "四轮测量"),
-        (0, "换水（开状态）")
+        (1, 90),
+        (0,-90),
     ]
     
     instructions = """
     请选择要发布的步骤：
-    1: 开状态
-    2: 一轮测量（关状态）
-    3: 换水
-    4: 二轮测量
-    5: 换水
-    6: 三轮测量
-    7: 换水
-    8: 四轮测量
-    9: 换水（开状态）
+    1: +90
+    2: -90
     输入对应的数字然后按Enter键:
     """
-    
+    angle=0
     while not rospy.is_shutdown():
         print(instructions)
-        command = input("请输入步骤 (1/2/3/4/5/6/7/8/9): ")
+        command = input("请输入步骤 (1/2): ")
         
-        if command in map(str, range(1, 10)):
+        if command in map(str, range(1, 3)):
             step_index = int(command) - 1
             absolute_position, description = steps[step_index]
-            rospy.loginfo(f"当前绝对位置: {absolute_position}")
-            rospy.loginfo(f"描述: {description}")
+            rospy.loginfo(f"相对位置指令: {absolute_position}")
+            angle=angle+description
+            rospy.loginfo(f"绝对位置: {angle}")
             position_pub.publish(absolute_position)
-            description_pub.publish(description)
+            description_pub.publish(angle)
         else:
             rospy.logwarn("无效的命令，请输入 1 到 9 之间的数字")
         
--- Ros_Repeater/src/motion/scripts/Tuogou_sub.py
+++ Ros_Repeater/src/motion/scripts/Tuogou_sub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
-
+import threading
 # 确保当前脚本目录在 sys.path 中
 script_dir = os.path.dirname(__file__)
 if script_dir not in sys.path:
@@ -10,26 +10,51 @@ if script_dir not in sys.path:
 from Motor_class import CANMotorController
 
 import rospy
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool,Float32
 import time
 import logging
 
 # 设置日志记录
 log_file = os.path.join(script_dir, 'motor_control.log')
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-class MotorControlNode:
+controller = CANMotorController(motor_id=0x00)
+controller.open_device()
+controller.init_can_channel()
+controller.start_can_channel()
+class MotorControlNode2:
     def __init__(self):
-        rospy.init_node('Tuogou_control_node', anonymous=True)
         
         # 初始化CANMotorController对象
-        self.controller = CANMotorController(motor_id=0x03)
-        
-        # 开启设备
-        self.controller.open_device()
-        self.controller.init_can_channel()
-        self.controller.start_can_channel()
+        self.ID=0x03
 
+        # 订阅话题
+        rospy.Subscriber('/absolute_description', Float32, self.control_callback)
+        
+
+
+    def control_callback(self, msg):
+
+        angle = msg.data
+        print("angleangleangleangle",angle)
+        logging.info(f'Received control command: {"ON" if msg.data else "OFF"}, setting angle to {angle}')
+        
+
+        controller.send_angle(self.ID, angle)
+        data = controller.receive_data()
+        controller.parse_received_data(data)
+
+
+
+    def on_shutdown(self):
+        # 确保设备关闭
+        controller.close_device()
+        logging.info('CAN device closed.')
+
+    def run(self):
+        rospy.spin()
+class MotorControlNode:
+    def __init__(self):
+        self.ID=0x04   
         # 订阅话题
         rospy.Subscriber('Tuogou_control', Bool, self.control_callback)
         
@@ -45,19 +70,17 @@ class MotorControlNode:
         
         for _ in range(2):
             # 第一次发送角度指令
-            self.controller.send_angle(self.controller.motor_id, angle)
-            data = self.controller.receive_data()
-            self.controller.parse_received_data(data)
+            controller.send_angle(self.ID, angle)
+            data = controller.receive_data()
+            controller.parse_received_data(data)
 
             # 等待一段时间
             time.sleep(1)  # 根据需要调整时间间隔
         
-        # 关闭节点
-        rospy.signal_shutdown('Task completed and shutting down.')
 
     def on_shutdown(self):
         # 确保设备关闭
-        self.controller.close_device()
+        controller.close_device()
         logging.info('CAN device closed.')
 
     def run(self):
@@ -65,8 +88,26 @@ class MotorControlNode:
 
 
 if __name__ == '__main__':
+    rospy.init_node('Tuogou_control_node', anonymous=True)
+    # 创建 MotorControlNode 实例
     motor_control_node = MotorControlNode()
-    motor_control_node.run()
+    
+    # 创建 MotorControlNode2 实例
+    motor_control_node2 = MotorControlNode2()
+    
+    # 创建线程来运行 MotorControlNode
+    thread1 = threading.Thread(target=motor_control_node.run)
+    
+    # 创建线程来运行 MotorControlNode2
+    thread2 = threading.Thread(target=motor_control_node2.run)
+    
+    # 启动两个线程
+    thread1.start()
+    thread2.start()
+    
+    # 等待两个线程完成
+    thread1.join()
+    thread2.join()
     try:
         pass
         #motor_control_node = MotorControlNode()
